158e12bc734d5f88060af700bf2361fa58860d38
dim8art <dim8art@yandex.ru>
2025-04-06 17:06:16 +0300

    added handler structure

---
+++ telegram_bot/handlers/main.py
@@ -0,0 +1,12 @@
+from aiogram import Dispatcher, Router
+
+from register import register_router
+
+
+def register_all_handlers(dp: Dispatcher) -> None:
+    main_router = Router()
+
+    main_router.include_routers()
+    main_router.callback_query.outer_middleware(register_router)
+
+    dp.include_router(main_router)
