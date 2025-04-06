42b83a4d4514a5f9a09394284a8698c21cec1e2a
dim8art <dim8art@yandex.ru>
2025-04-06 16:43:28 +0300

    added handler structure

---
+++ telegram_bot/handlers/register/register.py
@@ -0,0 +1,16 @@
+# '/start'
+from aiogram import Router, F, types
+from aiogram.enums import ContentType, ParseMode
+from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.types import FSInputFile
+from state_machines import States_registration
+import l10n
+register_router = Router()
+
+@register_router.message(CommandStart())
+async def start(msg: types.Message, state: FSMContext):
+    await msg.answer(l10n.hi())
+    await msg.answer(l10n.ask_name())
+    await msg.answer(l10n.talk_about_pc())
+    await state.set_state(States_registration.name_waiting)
