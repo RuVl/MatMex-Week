43e183bd9bc47cb4130de3929acf8d964edf01e8
dim8art <dim8art@yandex.ru>
2025-04-07 14:01:05 +0300

    registartion handlers created

--- telegram_bot/keyboards/register.py
+++ telegram_bot/keyboards/register.py
@@ -8,3 +8,9 @@ def yes_no_kb() -> ReplyKeyboardMarkup:
     kb.row(KeyboardButton(text="Да"), 
            KeyboardButton(text="Нет"))
     return kb.as_markup(resize_keyboard=True)
+
+def manual_check_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.row(KeyboardButton(text="Отправить на ручную проверку"), 
+           KeyboardButton(text="Нет, я пошутил"))
+    return kb.as_markup(resize_keyboard=True)
