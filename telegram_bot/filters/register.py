8c9db06ff789a208e3f61a0ef0876a42fee5c4b8
dim8art <dim8art@yandex.ru>
2025-04-07 22:41:26 +0300

    small snages

--- telegram_bot/filters/register.py
+++ telegram_bot/filters/register.py
@@ -8,11 +8,10 @@ from database.methods import get_user_by_telegram_id
 
 class FIO_filter(BaseFilter):
 	async def __call__(self, message: Message) -> bool:
-		fio_pattern = re.compile(r'^([А-Яа-яЁё]+(-[А-Яа-яЁё]+)?\s){2}[А-Яа-яЁё]+(-[А-Яа-яЁё]+)?$')
+		fio_pattern = re.compile(r'^[А-ЯЁа-яё]+(?:[-][А-ЯЁа-яё]+)*\s+[А-ЯЁа-яё]+(?:[-][А-ЯЁа-яё]+)*\s*(?:[А-ЯЁа-яё]+(?:[-][А-ЯЁа-яё]+)*)?$')
 		return fio_pattern.match(message.text.strip())
 
 class is_not_registered_filter():
 	async def __call__(self, message: Message) -> bool:
 		async with async_session() as session:
 			return await get_user_by_telegram_id(session, message.from_user.id) == None
-	  
