8ef31437b2347f26cd943574b1827247aa273048
dim8art <dim8art@yandex.ru>
2025-04-15 06:11:24 +0300

    Перенес удаление в админку

--- telegram_bot/handlers/admin/edit_shop.py
+++ telegram_bot/handlers/admin/edit_shop.py
@@ -7,12 +7,12 @@ from fluent.runtime import FluentLocalization
 from structlog.typing import FilteringBoundLogger
 
 from keyboards.common import get_edit_shop_kb, get_cancel_kb, get_admin_kb, get_category_kb, get_item_size_kb, get_yes_no_cancel_kb
-from keyboards.inline import get_category_ikb, get_item_ikb
+from keyboards.inline import get_category_ikb, get_item_ikb, ShopDeleteCategoryFactory, ShopDeleteItemFactory
 from state_machines.states_admin import AdminActions
 from state_machines.states_edit_shop import EditShopActions
 from filters.main import LocalizedTextFilter
 from config import MEDIA_DIR
-from database.methods import create_category, get_category_by_name, create_item
+from database.methods import create_category, get_category_by_name, create_item, remove_item_by_id, remove_category_by_id, get_category_by_id
 from database import async_session
 from database.enums import MerchSize
 edit_shop_router = Router()
@@ -91,19 +91,6 @@ async def handle_create_category(msg: types.Message, state: FSMContext, l10n: Fl
 		return
 	await msg.answer(l10n.format_value("category-created"), reply_markup=get_edit_shop_kb(l10n))
 	await state.set_state(EditShopActions.EDIT_SHOP)
-	await log.adebug("log-state-changed", state=EditShopActions.EDIT_SHOP.state)
-
-@edit_shop_router.message(EditShopActions.EDIT_SHOP, LocalizedTextFilter("btn-delete-item-or-category"))
-async def handle_delete_category_btn(msg: types.Message, state: FSMContext, l10n: FluentLocalization, log: FilteringBoundLogger):
-	await log.adebug("log-admin-action", action="handle_delete_category_btn")	
-	text = l10n.format_value("shop-hello")
-	image_from_pc = types.FSInputFile(MEDIA_DIR / "shop_mock.jpg")
-	category_ikb = await get_category_ikb(l10n, True)
-	await msg.answer_photo(
-		image_from_pc,
-		caption=text,
-		reply_markup=category_ikb
-	)
 	await log.adebug("log-state-changed", state=EditShopActions.EDIT_SHOP.state)
 
 @edit_shop_router.message(EditShopActions.EDIT_SHOP, LocalizedTextFilter("btn-add-item"))
@@ -220,3 +207,47 @@ async def handle_ask_for_item_image(msg: types.Message, state: FSMContext, l10n:
 	await state.clear()
 	await state.set_state(EditShopActions.EDIT_SHOP)
 	await log.adebug("log-state-changed", state=EditShopActions.EDIT_SHOP.state)
+
+@edit_shop_router.message(EditShopActions.EDIT_SHOP, LocalizedTextFilter("btn-delete-item-or-category"))
+async def handle_delete_category_btn(msg: types.Message, state: FSMContext, l10n: FluentLocalization, log: FilteringBoundLogger):
+	await log.adebug("log-admin-action", action="handle_delete_category_btn")	
+	text = l10n.format_value("shop-hello")
+	image_from_pc = types.FSInputFile(MEDIA_DIR / "shop_mock.jpg")
+	category_ikb = await get_category_ikb(l10n, True)
+	await msg.answer_photo(
+		image_from_pc,
+		caption=text,
+		reply_markup=category_ikb
+	)
+	await log.adebug("log-state-changed", state=EditShopActions.EDIT_SHOP.state)
+
+@edit_shop_router.callback_query(ShopDeleteItemFactory.filter(F))
+async def handle_choose_category(callback: types.CallbackQuery, l10n: FluentLocalization):
+	data = ShopDeleteItemFactory.unpack(callback.data)
+	async with async_session() as session:	
+		if data.can_delete:
+			await remove_item_by_id(session, data.item_id)
+	async with async_session() as session:
+		category = await get_category_by_id(session, data.category_id)
+	
+	await callback.bot.edit_message_media(
+     									media = types.InputMediaPhoto(media = types.FSInputFile(category.image_path),
+																caption = l10n.format_value("ask-for-item-name")),
+              							reply_markup=get_item_ikb(l10n, category, data.can_delete),
+                                    	chat_id=callback.message.chat.id,
+        								message_id=callback.message.message_id)
+
+@edit_shop_router.callback_query(ShopDeleteCategoryFactory.filter(F))
+async def handle_delete_category(callback: types.CallbackQuery, l10n: FluentLocalization):
+	data = ShopDeleteCategoryFactory.unpack(callback.data)
+	async with async_session() as session:	
+		if data.can_delete:
+			await remove_category_by_id(session, data.category_id)
+	image_from_pc = types.FSInputFile(MEDIA_DIR / "shop_mock.jpg")
+	category_ikb = await get_category_ikb(l10n, data.can_delete)
+	await callback.bot.edit_message_media(
+     									media = types.InputMediaPhoto(media = image_from_pc,
+																caption = l10n.format_value("shop-hello")),
+              							reply_markup=category_ikb,
+                                    	chat_id=callback.message.chat.id,
+        								message_id=callback.message.message_id)
--- telegram_bot/handlers/user/shop.py
+++ telegram_bot/handlers/user/shop.py
@@ -42,40 +42,9 @@ async def handle_choose_category(callback: types.CallbackQuery, l10n: FluentLoca
                                     	chat_id=callback.message.chat.id,
         								message_id=callback.message.message_id)
 
-@shop_router.callback_query(ShopDeleteItemFactory.filter(F))
-async def handle_choose_category(callback: types.CallbackQuery, l10n: FluentLocalization):
-	data = ShopDeleteItemFactory.unpack(callback.data)
-	async with async_session() as session:	
-		if data.can_delete:
-			await remove_item_by_id(session, data.item_id)
-	async with async_session() as session:
-		category = await get_category_by_id(session, data.category_id)
-	
-	await callback.bot.edit_message_media(
-     									media = InputMediaPhoto(media = FSInputFile(category.image_path),
-																caption = l10n.format_value("ask-for-item-name")),
-              							reply_markup=get_item_ikb(l10n, category, data.can_delete),
-                                    	chat_id=callback.message.chat.id,
-        								message_id=callback.message.message_id)
-
 @shop_router.callback_query(ShopBackToCategoriesFactory.filter(F))
 async def handle_back_to_categories(callback: types.CallbackQuery, l10n: FluentLocalization):
 	data = ShopBackToCategoriesFactory.unpack(callback.data)
-	image_from_pc = FSInputFile(MEDIA_DIR / "shop_mock.jpg")
-	category_ikb = await get_category_ikb(l10n, data.can_delete)
-	await callback.bot.edit_message_media(
-     									media = InputMediaPhoto(media = image_from_pc,
-																caption = l10n.format_value("shop-hello")),
-              							reply_markup=category_ikb,
-                                    	chat_id=callback.message.chat.id,
-        								message_id=callback.message.message_id)
-
-@shop_router.callback_query(ShopDeleteCategoryFactory.filter(F))
-async def handle_delete_category(callback: types.CallbackQuery, l10n: FluentLocalization):
-	data = ShopDeleteCategoryFactory.unpack(callback.data)
-	async with async_session() as session:	
-		if data.can_delete:
-			await remove_category_by_id(session, data.category_id)
 	image_from_pc = FSInputFile(MEDIA_DIR / "shop_mock.jpg")
 	category_ikb = await get_category_ikb(l10n, data.can_delete)
 	await callback.bot.edit_message_media(
