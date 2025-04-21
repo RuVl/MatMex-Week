e91c69375c17ce002a121a8d9a6d39b57c4d36c6
ruvl <ru.vlad.13@gmail.com>
2025-04-21 06:50:51 +0300

    Locale and logging changes

--- telegram_bot/handlers/user/account.py
+++ telegram_bot/handlers/user/account.py
@@ -4,23 +4,23 @@ from fluent.runtime import FluentLocalization
 from structlog.typing import FilteringBoundLogger
 
 from database import async_session
-from database.methods import get_user_by_telegram_id, update_user_fullname
+from database.enums import ApplyStatus
+from database.methods import update_user_fullname
+from database.models import User
 from filters import FullNameFilter, LocalizedTextFilter
 from keyboards.common import account_menu_kb, cancel_kb, menu_kb
 from state_machines.account import AccountActions
+from utils import escape_md_v2
 
 account_router = Router()
 
 
-@account_router.message(LocalizedTextFilter("btn-profile"))
-async def profile_open_h(msg: types.Message, state: FSMContext, l10n: FluentLocalization):
-	async with async_session() as session:
-		user = await get_user_by_telegram_id(session, msg.from_user.id)
-
+@account_router.message(LocalizedTextFilter("btn-profile"), flags={'no_cache': True})
+async def profile_open_h(msg: types.Message, state: FSMContext, cached_user: User, l10n: FluentLocalization):
 	await msg.answer(l10n.format_value("welcome-account", args={
-		'fullname': user.full_name,
-		'balance': user.balance,
-		'in_pc': user.apply is not None,
+		'fullname': escape_md_v2(cached_user.full_name),
+		'balance': cached_user.balance,
+		'in_pc': cached_user.apply.status == ApplyStatus.APPROVED,
 	}), reply_markup=account_menu_kb(l10n))
 
 	# TODO добавить купленные товары
@@ -40,19 +40,14 @@ async def edit_name_cancel_h(msg: types.Message, state: FSMContext, l10n: Fluent
 
 
 @account_router.message(AccountActions.NAME_WAITING, FullNameFilter())
-async def edit_name_submit_h(msg: types.Message, state: FSMContext, l10n: FluentLocalization, log: FilteringBoundLogger):
-	await log.adebug("log-name-changed", new_name=msg.text.strip())
+async def edit_name_submit_h(msg: types.Message, state: FSMContext, cached_user: User, l10n: FluentLocalization, log: FilteringBoundLogger):
 	new_name = msg.text.strip()
 
+	await log.adebug("log-name-changed", new_name=new_name)
 	async with async_session() as session:
-		user = await get_user_by_telegram_id(session, msg.from_user.id)
-		if user:
-			await update_user_fullname(session, user.id, new_name)
-			await log.adebug("log-user-data-updated", field="full_name", value=new_name)
-		else:
-			await log.aerror("user-not-found", telegram_id=msg.from_user.id)
+		await update_user_fullname(session, cached_user.id, new_name)
 
-	await msg.answer(l10n.format_value("name-changed", args={'fullname': new_name}), reply_markup=account_menu_kb(l10n))
+	await msg.answer(l10n.format_value("name-changed", args={'fullname': escape_md_v2(new_name)}), reply_markup=account_menu_kb(l10n))
 	await state.set_state(AccountActions.ACCOUNT_PANEL)
 
 
@@ -68,6 +63,6 @@ async def already_in_pc_h(msg: types.Message, l10n: FluentLocalization):
 
 @account_router.message(AccountActions.ACCOUNT_PANEL, LocalizedTextFilter("btn-back-to-menu"))
 async def back_to_menu_h(msg: types.Message, state: FSMContext, l10n: FluentLocalization):
-	menu = await menu_kb(l10n, msg.from_user.id)
-	await msg.answer(l10n.format_value("back-to-menu"), reply_markup=menu)
+	kb = await menu_kb(l10n, msg.from_user.id)
+	await msg.answer(l10n.format_value("back-to-menu"), reply_markup=kb)
 	await state.clear()
